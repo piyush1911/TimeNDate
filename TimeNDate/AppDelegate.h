551f86bb87f0101b10c009d1a572b637e7696488
Piyush Sharma <mailpiyushsharma@gmail.com>
2015-09-15 13:35:42 -0700

    Initial Commit

---
+++ TimeNDate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeNDate
+//
+//  Created by Piyush Sharma on 9/15/15.
+//  Copyright (c) 2015 Piyush Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
